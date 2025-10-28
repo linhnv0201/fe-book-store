d022911d6b43f107b0b416b76330bf1e378d3c47
Vu Linh Dev <linh.nv0201@gmail.com>
2025-10-28 16:32:04 +0700

    footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,36 +2,39 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HeaderLayoutComponent } from './shared/header-layout/header-layout.component';
+import { FooterLayoutComponent } from './shared/footer-layout/footer-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule],
+  imports: [RouterOutlet, HeaderLayoutComponent, FooterLayoutComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  //text
-  test = 'hello-world';
-  title = {
-    name: "Linh",
-    age: "23"
-  }
 
-  //properties
-  isDisable = true;
 
-  //attributes
-  contentImage = "Nhã Linh welcome";
-  nameBtn = "Thử ấn xem";
-  clickMeMessage = '';
-  bindingMessage = '';
-  handleClickMe(): void {
-    this.clickMeMessage= "Suprise!!";
-  }
+  // //text
+  // test = 'hello-world';
+  // title = {
+  //   name: "Linh",
+  //   age: "23"
+  // }
 
-  updateField(): void {
-    console.log("hello console");
-  }
+  // //properties
+  // isDisable = true;
+
+  // //attributes
+  // contentImage = "Nhã Linh welcome";
+  // nameBtn = "Thử ấn xem";
+  // clickMeMessage = '';
+  // bindingMessage = '';
+  // handleClickMe(): void {
+  //   this.clickMeMessage= "Suprise!!";
+  // }
+
+  // updateField(): void {
+  //   console.log("hello console");
+  // }
 
 }
---
+++ src/app/shared/footer-layout/footer-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'footer-layout',
+    standalone: true,
+    templateUrl: './footer-layout.component.html',
+    styleUrl: './footer-layout.component.css',
+})
+export class FooterLayoutComponent {}
