5aa52cc90ffeb140fb9b04800159ce87c2691798
Vu Linh Dev <linh.nv0201@gmail.com>
2025-10-28 15:35:14 +0700

    fe for bookstore, only have header ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HeaderLayoutComponent } from './shared/header-layout/header-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'bookstore-fe';
+  //text
+  test = 'hello-world';
+  title = {
+    name: "Linh",
+    age: "23"
+  }
+
+  //properties
+  isDisable = true;
+
+  //attributes
+  contentImage = "Nhã Linh welcome";
+  nameBtn = "Thử ấn xem";
+  clickMeMessage = '';
+  bindingMessage = '';
+  handleClickMe(): void {
+    this.clickMeMessage= "Suprise!!";
+  }
+
+  updateField(): void {
+    console.log("hello console");
+  }
+
 }
