4741abf7127a55de4628fb2f72588e652a73b5a5
Vu Linh Dev <linh.nv0201@gmail.com>
2025-10-29 09:22:02 +0700

    first route for home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { HeaderLayoutComponent } from './shared/header-layout/header-layout.component';
 import { FooterLayoutComponent } from './shared/footer-layout/footer-layout.component';
-import { CurrencyPipe } from './shared/pipes/CurrencyPipe.pipe';
-import { UpperCasePipe } from './shared/pipes/UpperCasePipe.pipe';
-import { NgFor } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderLayoutComponent, FooterLayoutComponent,
-     FormsModule, CurrencyPipe, UpperCasePipe, NgFor],
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-
-  products = [
-    {name: 'cuon theo chieu gio', price: 120000, image: "assets/images/product1.jpg"},
-    {name: 'sach2', price: 120000, image: "assets/images/product1.jpg"},
-    {name: 'sach3', price: 120000, image: "assets/images/product1.jpg"},
-    {name: 'sach4', price: 120000, image: "assets/images/product1.jpg"},
-  ]
-
-
-  // //text
-  // test = 'hello-world';
-  // title = {
-  //   name: "Linh",
-  //   age: "23"
-  // }
-
-  // //properties
-  // isDisable = true;
-
-  // //attributes
-  // contentImage = "Nhã Linh welcome";
-  // nameBtn = "Thử ấn xem";
-  // clickMeMessage = '';
-  // bindingMessage = '';
-  // handleClickMe(): void {
-  //   this.clickMeMessage= "Suprise!!";
-  // }
-
-  // updateField(): void {
-  //   console.log("hello console");
-  // }
 
 }
